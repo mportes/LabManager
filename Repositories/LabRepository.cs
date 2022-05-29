c0c6967c320f918ec174ef61097ab58f52dd1c58
Matheus Portes <mportesif@gmail.com>
2022-05-29 14:54:39 -0300

    add lab delete, update and show functionalities

--- Repositories/LabRepository.cs
+++ Repositories/LabRepository.cs
@@ -51,4 +51,62 @@ class LabRepository
         connection.Close();
         return lab;
     }
+
+    public Lab Update(Lab lab)
+    {
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = @"
+            UPDATE Labs 
+            SET 
+                number = $number,
+                name = $name,
+                block = $block
+            WHERE id = $id
+        ";
+        command.Parameters.AddWithValue("$id", lab.Id);
+        command.Parameters.AddWithValue("$number", lab.Number);
+        command.Parameters.AddWithValue("$name", lab.Name);
+        command.Parameters.AddWithValue("$block", lab.Block);
+
+        command.ExecuteNonQuery();
+        connection.Close();
+        return lab;
+    }
+
+    public void Delete(int id)
+    {
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = "DELETE FROM Labs WHERE id = $id";
+        command.Parameters.AddWithValue("$id", id);
+
+        command.ExecuteNonQuery();
+        connection.Close();
+    }
+
+    public Lab GetById(int id)
+    {
+        var connection = new SqliteConnection(_databaseConfig.ConnectionString);
+        connection.Open();
+
+        var command = connection.CreateCommand();
+        command.CommandText = "SELECT * FROM Labs WHERE id = $id";
+        command.Parameters.AddWithValue("$id", id);
+
+        var reader = command.ExecuteReader();
+        reader.Read();
+
+        var number = reader.GetString(1);
+        var name = reader.GetString(2);
+        var block = reader.GetString(3);
+        var lab = new Lab(id, number, name, block);
+        
+        connection.Close();
+        return lab;
+    }
 }
